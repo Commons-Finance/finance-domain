77cb5a198c58e7fa26411f1907396b3fcd403b2e
mharris021 <mharris021@hotmail.com>
2015-12-05 17:57:03 -0600

    Refactored Test Configurations to make test dependencies more clearly defined

--- src/test/groovy/org/darcstarsolutions/common/finance/domain/calculators/config/StandardBondCalculatorTestConfiguration.groovy
+++ src/test/groovy/org/darcstarsolutions/common/finance/domain/calculators/config/StandardBondCalculatorTestConfiguration.groovy
@@ -1,5 +1,7 @@
 package org.darcstarsolutions.common.finance.domain.calculators.config
 import org.darcstarsolutions.common.finance.domain.CompoundingEngine
+import org.darcstarsolutions.common.finance.domain.CompoundingPeriod
+import org.darcstarsolutions.common.finance.domain.CompoundingType
 import org.darcstarsolutions.common.finance.domain.calculators.BondCalculator
 import org.darcstarsolutions.common.finance.domain.calculators.StandardBondCouponCalculator
 import org.darcstarsolutions.common.finance.domain.calculators.YieldCalculator
@@ -7,31 +9,33 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-
-import javax.annotation.Resource
 /**
  * Created by mharris on 12/2/15.
  */
 
 @Configuration
-@Import(value = [CompoundingTestConfiguration.class, YieldCalculatorTestConfiguration.class, CouponCalculatorTestConfiguration.class])
 class StandardBondCalculatorTestConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(StandardBondCalculatorTestConfiguration.class)
 
-    @Resource
-    CompoundingEngine simpleAnnuallyCompoundingEngine
+    @Bean
+    CompoundingEngine simpleAnnuallyCompoundingEngine() {
+        return new CompoundingEngine(CompoundingType.SIMPLE, CompoundingPeriod.ANNUALLY)
+    }
 
-    @Resource
-    YieldCalculator zeroCouponBondYieldCalculator
+    @Bean(name = "zeroCouponBondYieldCalculator")
+    YieldCalculator zeroCouponBondYieldCalculator() {
+        return new YieldCalculator(simpleAnnuallyCompoundingEngine())
+    }
 
-    @Resource
-    StandardBondCouponCalculator standardBondCouponCalculator
+    @Bean(name = "standardBondCouponCalculator")
+    StandardBondCouponCalculator standardBondCouponCalculator() {
+        return new StandardBondCouponCalculator()
+    }
 
     @Bean(name = "standardBondCalculator")
     BondCalculator standardBondCalculator() {
-        return new BondCalculator(simpleAnnuallyCompoundingEngine, zeroCouponBondYieldCalculator, standardBondCouponCalculator)
+        return new BondCalculator(simpleAnnuallyCompoundingEngine(), zeroCouponBondYieldCalculator(), standardBondCouponCalculator())
     }
 
 }
--- src/test/groovy/org/darcstarsolutions/common/finance/domain/calculators/config/ZeroCouponBondCalculatorTestConfiguration.groovy
+++ src/test/groovy/org/darcstarsolutions/common/finance/domain/calculators/config/ZeroCouponBondCalculatorTestConfiguration.groovy
@@ -1,6 +1,7 @@
 package org.darcstarsolutions.common.finance.domain.calculators.config
-
 import org.darcstarsolutions.common.finance.domain.CompoundingEngine
+import org.darcstarsolutions.common.finance.domain.CompoundingPeriod
+import org.darcstarsolutions.common.finance.domain.CompoundingType
 import org.darcstarsolutions.common.finance.domain.calculators.BondCalculator
 import org.darcstarsolutions.common.finance.domain.calculators.YieldCalculator
 import org.darcstarsolutions.common.finance.domain.calculators.ZeroCouponBondCouponCalculator
@@ -8,32 +9,33 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-
-import javax.annotation.Resource
-
 /**
  * Created by mharris on 12/2/15.
  */
 
 @Configuration
-@Import(value = [CompoundingTestConfiguration.class, YieldCalculatorTestConfiguration.class, CouponCalculatorTestConfiguration.class])
 class ZeroCouponBondCalculatorTestConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(ZeroCouponBondCalculatorTestConfiguration.class)
 
-    @Resource
-    CompoundingEngine simpleAnnuallyCompoundingEngine
+    @Bean
+    CompoundingEngine simpleAnnuallyCompoundingEngine() {
+        return new CompoundingEngine(CompoundingType.SIMPLE, CompoundingPeriod.ANNUALLY)
+    }
 
-    @Resource
-    YieldCalculator zeroCouponBondYieldCalculator
+    @Bean(name = "zeroCouponBondYieldCalculator")
+    YieldCalculator zeroCouponBondYieldCalculator() {
+        return new YieldCalculator(simpleAnnuallyCompoundingEngine())
+    }
 
-    @Resource
-    ZeroCouponBondCouponCalculator zeroCouponBondCouponCalculator
+    @Bean(name = "zeroCouponBondCouponCalculator")
+    ZeroCouponBondCouponCalculator zeroCouponBondCouponCalculator() {
+        return new ZeroCouponBondCouponCalculator()
+    }
 
     @Bean(name = "zeroCouponBondCalculator")
     BondCalculator zeroCouponBondCalculator() {
-        return new BondCalculator(simpleAnnuallyCompoundingEngine, zeroCouponBondYieldCalculator, zeroCouponBondCouponCalculator)
+        return new BondCalculator(simpleAnnuallyCompoundingEngine(), zeroCouponBondYieldCalculator(), zeroCouponBondCouponCalculator())
     }
 
 }
